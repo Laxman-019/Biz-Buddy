ac68e05e9299e902e018ef48fb904fa08ed30e3c
Jitendra Maharana <deepakmaharana3500@gmail.com>
2026-02-03 00:18:15 +0530

    Business Summary & Monthly Summary api

--- backend/businesses/urls.py
+++ backend/businesses/urls.py
@@ -4,4 +4,6 @@ from businesses.views import *
 urlpatterns = [
     path('add-record/', add_record),
     path('list-record/', list_records),
+    path('business-summary/', business_summary),
+    path('monthly-summary/', monthly_summary),
 ]
